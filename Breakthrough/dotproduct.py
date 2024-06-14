644f17576fd13b683ff09018a0e1adeae823a39d
Adam W. <adamw@tutamail.com>
2024-06-13 23:09:10 -0400

    almost done!

--- Breakthrough/dotproduct.py
+++ Breakthrough/dotproduct.py
@@ -22,15 +22,72 @@ class QK(Scene):
         sentenceQ = Tex("He ", "swung ", "the ", "baseball ", "bat ").scale(1).set_color_by_tex("He", ORANGE).set_color_by_tex("swung", RED).set_color_by_tex("the", YELLOW).set_color_by_tex("baseball", GREEN).set_color_by_tex("bat", BLUE)
         sentenceQ.arrange(RIGHT, buff=0.55).next_to(queryMatrix, UP, buff=0.3)
 
+        rectangleWord = []
+
         for i in range(0, 5):
             rectangles.add(Rectangle(width=(sentence1[i].width+0.3), height=(sentence1[i].height+0.3), color=sentence1[i].color).move_to(sentence1[i]))
             rectangles.add(Rectangle(width=(sentenceQ[i].width+0.3), height=(sentenceQ[i].height+0.3), color=sentenceQ[i].color).move_to(sentenceQ[i]))
 
+            rectangleWord.append(VGroup(sentenceQ[i], rectangles[i*2+1]))
+            rectangleWord.append(VGroup(sentence1[i], rectangles[i*2]))
 
-        compatabilityText = MathTex("Compatibility(     ,     )").move_to(rectangles[1]).shift(LEFT*4.9, DOWN*0.5).scale(0.75)
+        table = Table([
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"]], v_buff=1.6, h_buff=3.25).scale(0.42).move_to(queryMatrix.get_center()).shift(DOWN*3.42).set_opacity(0.7)
+        # .set_color_by_gradient([BLUE, GREEN, YELLOW, RED, ORANGE])
+        table.get_rows()[0:5].set_opacity(0)
 
-        self.add(queryMatrix, keyMatrixTransposed, sentence1, rectangles, sentenceQ, compatabilityText)
 
+        background = Rectangle(height=2.85, width=5.6).move_to(queryMatrix).shift(LEFT*7, UP*0.42)
+        background.set_fill(opacity=.10)
+        background.set_stroke(opacity=0.9)
+        background.set_color(WHITE)
+
+
+        compatabilityText = MathTex(r"Compatibility(\qquad\qquad ,\qquad\qquad)=").scale(0.85).move_to(rectangles[1]).shift(LEFT*3.76, UP*0.07).scale(0.75)
+
+        queryColumns = queryMatrix.get_columns()
+        keyRows = keyMatrixTransposed.get_rows()
+
+        blankMatrixQuery = Matrix([[0.0], [0.0], ["..."], [0.0]], h_buff=3).scale(0.58).move_to(compatabilityText).shift(DOWN*1.35, LEFT*2.15)
+        blankMatrixQuery.get_columns()[0].set_opacity(0)
+
+        blankMatrixKey = Matrix([[0.0, 0.0, "...", 0.0]], v_buff=1.7).scale(0.58).next_to(blankMatrixQuery, RIGHT, 0.1)
+        blankMatrixKey.get_rows()[0].set_opacity(0)
+
+        equals = MathTex("=").next_to(blankMatrixKey, RIGHT, 0.15).scale(0.7)
+
+        attentionPattern = VGroup(
+            MathTex("3.5"), MathTex("-1.1"), MathTex("-3.5"), MathTex("-0.1"), MathTex("9.1"),
+            MathTex("7.5"), MathTex("-2.1"), MathTex("-2.5"), MathTex("-0.2"), MathTex("9.2"),
+            MathTex("1.5"), MathTex("-3.1"), MathTex("-1.5"), MathTex("-0.3"), MathTex("9.3"),
+            MathTex("6.5"), MathTex("-4.1"), MathTex("-0.5"), MathTex("-0.4"), MathTex("9.4"),
+            MathTex("2.5"), MathTex("-5.1"), MathTex("-6.5"), MathTex("-0.6"), MathTex("9.5"),
+        )
+
+        self.add(background)
+        self.add(queryMatrix, keyMatrixTransposed, sentence1, rectangles, sentenceQ, compatabilityText, blankMatrixKey, blankMatrixQuery, table, equals)
+        for i in range(0,5):
+            workingQueryText = rectangleWord[2*i].copy()
+            workingQueryMatrix = queryColumns[i].copy()
+            self.play(workingQueryText.scale(0.6).animate.move_to(compatabilityText).shift(RIGHT*0.11), workingQueryMatrix.animate.move_to(blankMatrixQuery), run_time=0.6)
+            for j in range(0,5):
+                workingKeyText = rectangleWord[2*j+1].copy()
+                workingKeyMatrix = keyRows[j].copy()
+                self.play(LaggedStart(
+                    AnimationGroup(
+                        workingKeyText.scale(0.6).animate.move_to(compatabilityText).shift(RIGHT*1.5), 
+                        workingKeyMatrix.animate.move_to(blankMatrixKey), 
+                    ),
+                    Write(attentionPattern[j*5+i].scale(0.5).next_to(equals, RIGHT, 0.22)),
+                    lag_ratio=0.5
+                ), run_time=1)
+                self.play(attentionPattern[j*5+i].animate.scale(1.2).move_to(table.get_cell(pos=(j+1, i+1))), run_time=0.4)
+                self.remove(workingKeyText, workingKeyMatrix)
+            self.remove(workingQueryText, workingQueryMatrix)
         self.wait()
 
         
